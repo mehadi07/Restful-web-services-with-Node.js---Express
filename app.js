d829d1229dc3ee76472c434603f06eb7078ae9ff
mehadi07 <saimum22@gmail.com>
2017-05-21 22:35:10 -0400

    More

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 var express = require('express');
+    mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var book = require('./models/bookModel');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
+var bookRouter = express.Router();
+
+bookRouter.route('/books')
+    .get(function (req, res) {
+        var responseJson = {
+            hello: "This is my Api"
+        };
+
+        res.json(responseJson);
+    });
+
+app.use('/api', bookRouter);
+
+
+
+
+
+app.get('/', function (req, res) {
     res.send('Welcome to my API !!');
 });
 
-app.listen(port, function(){
+app.listen(port, function () {
     console.log('Gulp is Running on PORT : ' + port);
 });
